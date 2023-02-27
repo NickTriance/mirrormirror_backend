0997100256ae2b378d3edf64ca80e6b5f1c6ba6d
NickTriance <114621135+NickTriance@users.noreply.github.com>
2023-02-27 14:59:22 -0800

    working on eye color detection

---
+++ src/gpthandler.py
@@ -0,0 +1,2 @@
+#TODO: shit for dealing with GPT-3 goes in here. We are going to have a method for taking in information about a person and returning a single string, whether 
+#	   it be a complement or a roast. 
--- src/main.py
+++ src/main.py
@@ -29,56 +29,50 @@ def detect_eyes(_face):
 	return eyes
 
 #isolates the iris, and then uses a color histogram to determine the person's eye color
+#TODO: re-write using RGB values. Use the split method to get them. See https://docs.opencv.org/3.4/d8/dbc/tutorial_histogram_calculation.html for more info
 def get_eye_color(_eyes, _img):
-	for (x,y,w,h) in _eyes:
+	for (x,y,w,r) in _eyes:
 
 		#extract iris region
-		_iris = _img[y:y+h, x:x+w]
+		_iris = _img[y:y+r, x:x+w]
+		bgr_planes = cv2.split(_iris)
 
-		#in order to do a histogram, we need to use the HSV color space
-		hsv = cv2.cvtColor(_iris, cv2.COLOR_BGR2HSV)
+		hist_size = 256
+		hist_range = (0, hist_size)
 
-		#calculate histogram for iris color
-		h = cv2.calcHist([hsv],[0], None, [180], [0,180])
-		s = cv2.calcHist([hsv],[1], None, [180], [0,180])
-		v = cv2.calcHist([hsv],[2], None, [180], [0,180])
-		h = cv2.normalize(h, h, 0, 255, cv2.NORM_MINMAX)
-		s = cv2.normalize(s, s, 0, 255, cv2.NORM_MINMAX)
-		v = cv2.normalize(v, v, 0, 255, cv2.NORM_MINMAX)
+		accumulate = False
 
-		h = np.argmax(h)
-		s = np.argmax(s)
-		v = np.argmax(v)
-		_col = (h,s,v)
-		#_col = np.argmax(hist)
-		print(f"Eye color (HSV): {_col}")
+		#the image is in BGR instead of RGB because...idk I'd have to google it.
+		b_hist = cv2.calcHist(bgr_planes, [0], None, [hist_size], hist_range, accumulate=accumulate)
+		g_hist = cv2.calcHist(bgr_planes, [1], None, [hist_size], hist_range, accumulate=accumulate)
+		r_hist = cv2.calcHist(bgr_planes, [2], None, [hist_size], hist_range, accumulate=accumulate)
+
+		hist_h = 400
+		cv2.normalize(b_hist, b_hist, alpha=0, beta=hist_h, norm_type=cv2.NORM_MINMAX)
+		cv2.normalize(g_hist, g_hist, alpha=0, beta=hist_h, norm_type=cv2.NORM_MINMAX)
+		cv2.normalize(r_hist, r_hist, alpha=0, beta=hist_h, norm_type=cv2.NORM_MINMAX)
+
+		#todo: extract bgr values with the max on the histogram
+
 		return _col
-
-def hsv_to_rgb(h, s, v):
-	(r,g,b) = colorsys.hsv_to_rgb(h,s,v)
-
-
 
 #takes in HSV color code, and returns the closest HTML color name.
 def name_eye_color(_col):
 
-	#convert HSV color, assuming 100% saturation and value.
-	_rgb = hsv_to_rgb(_col[0], _col[1], _col[2])
-	print(f"Eye color (RGB): {_rgb}")
-
 	#predefined dictionary for eye colors
 	colors = {
-		#TODO: fill in the rgb values for colors
 		"brown" : (94, 49, 34),
 		"blue" : (69, 96, 139),
 		"green" : (129, 121, 31),
 		"hazel" : (131, 122, 91),
 		"gray" : (146, 146, 146)
 	}
+
 	min_distance = float("inf")
+
 	closest_color = None
 	for color, value in colors.items():
-		distance = math.sqrt(((_rgb[0] + value[0]) ** 2) + ((_rgb[1] + value[1]) ** 2) + ((_rgb[2] + value[2]) ** 2))
+		distance = math.sqrt(((_col[0] + value[0]) ** 2) + ((_col[1] + value[1]) ** 2) + ((_col[2] + value[2]) ** 2))
 		if distance < min_distance:
 			min_distance = distance
 			closest_color = color
@@ -98,6 +92,7 @@ def draw_rects(_img, _face, _eyes):
 
 
 #infinite loop to read camera input
+#TODO: this is temporarily getting video input from the webcam. In the final implementation we will use an image sent over from the app.
 while True:
 
 	img = get_image()
